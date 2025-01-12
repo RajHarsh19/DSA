1eabba723551ceec22a531093bb7921bd0984363
Harsh Raj <darksword7629@gmail.com>
2025-01-12 23:57:32 +0530

    Time: 0 ms (100%), Space: 9.3 MB (46.67%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         vector<string>ans;
+        if(digits.empty())return ans;
         string output;
         int index=0;
         string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
