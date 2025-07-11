f9121dc50af7c9123a1ce211a5b2bd2d1918651e
Harsh Raj <darksword7629@gmail.com>
2025-07-11 23:58:19 +0530

    Time: 3 ms (16.16%), Space: 18 MB (51.65%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        original_color = image[sr][sc]
+        
+        if original_color == color:
+            return image  # No need to fill if the color is the same
+
+        def dfs(r, c):
+            if (r < 0 or r >= len(image) or
+                c < 0 or c >= len(image[0]) or
+                image[r][c] != original_color):
+                return
+            
+            image[r][c] = color
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        dfs(sr, sc)
+        return image
